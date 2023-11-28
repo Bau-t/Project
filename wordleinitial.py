f071d3fa4452b020175a1d1aa71605fbb478ae0f
Bau-t <108375706+Bau-t@users.noreply.github.com>
2023-11-28 09:43:08 -0500

    Update wordleinitial.py
    correction to nicholas' initial code

--- wordleinitial.py
+++ wordleinitial.py
@@ -1,13 +1,14 @@
 import random 
 
 wordlist = ["wheel","basic","anger","carry","meant","rapid","lower","fight","acute","chase","yield","visit","women","north","chose"]
-picked_word = random.choice(wordlist)
 guess = input("We have chosen a five letter word. Enter your guess here: ")
-
 guess_count = 0
+picked_word = random.choice(wordlist)
+
 while guess_count < 5:
     if guess == picked_word:
         print("Correct",guess,"is correct!")
-    elif guess != picked_word:
+    else:
         print("Nice guess, but that is not correct. Try again.")
-        guess_count += 1
+        guess = input("We have chosen a five letter word. Enter your guess here: ")
+    guess_count += 1
